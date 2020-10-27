4a1eaf49c12cd0a686c02e1d562beb51fda19769
lauqe <lauqelol@gmail.com>
2020-10-27 10:59:14 -0400

    yo this is to test first commit from my end

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public Vector3 myPos;
+    public Transform myPlay;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        transform.position = myPlay.position + myPos;
+    }
+
+}
---
+++ Assets/move2d.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move2d : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+		transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
