842a71665c186bbd2b4ef795f562b74c39ea1092
AudreyBernierLarose <abernier@my.centennialcollege.ca>
2020-11-11 23:12:45 -0500

    Assets and underground part of the level

---
+++ Assets/Scripts/Reposition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            GameObject portal = GameObject.FindGameObjectWithTag("Portal");
+            
+            float newX, newY;
+            newX = portal.transform.position.x;
+            newY = portal.transform.position.y;
+            player.transform.position = new Vector2(newX, newY);
+
+        }
+    }
+    
+}
---
+++ Assets/Scripts/RespawnAfterDeath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnAfterDeath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject respawnPoint = GameObject.FindGameObjectWithTag("Respawn");
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            float newX, newY;
+            newX = respawnPoint.transform.position.x;
+            newY = respawnPoint.transform.position.y;
+
+            player.transform.position = new Vector2(newX, newY);
+            
+        }
+    }
+}
