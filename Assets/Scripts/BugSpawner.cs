a0cedb5fede2b2b0641f093a4d5602cfa19ec620
AudreyBernierLarose <abernier@my.centennialcollege.ca>
2020-11-18 20:30:03 -0500

    Second iteration underground scene

---
+++ Assets/Scripts/BugSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugSpawner : MonoBehaviour
+{
+    public GameObject bug, bugspawner;
+    private float timer = 0f;
+    private float waitTime = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= waitTime)
+        {
+            Instantiate(bug, bugspawner.transform.position, bugspawner.transform.rotation);
+            timer = 0.0f;
+        }
+        
+        
+        
+    }
+}
